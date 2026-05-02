b2751b7534b6cbcd6a94ccd2a69f3de7ce3e97c5
sagar514 <sagarkukreja4@gmail.com>
2026-05-03 02:36:00 +0530

    Update in console messages

--- user/src/db/config.js
+++ user/src/db/config.js
@@ -4,9 +4,9 @@ const connectToDB = async () => {
 
     try {
         await mongoose.connect(process.env.MONGO_URI);
-        console.log("Connected to DB");
+        console.log("User service: Connected to DB");
     } catch (error) {
-        console.log("Failed to connect to DB: ", error);
+        console.log("User service: Failed to connect to DB: ", error);
         process.exit(1);
     }
     
